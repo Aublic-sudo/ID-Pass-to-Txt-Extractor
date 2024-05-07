7485cd14944d5f97497142db643102bce4e3a1f7
kingskr62 <167851226+KINGSKR@users.noreply.github.com>
2024-05-07 12:56:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28
-api_hash = "eb86e"
-bot_token = ""
-auth_users = "18773"
+api_id = 26209173
+api_hash = "a4d86637bb1b8db35e92196493798279"
+bot_token = "6477606611:AAHrT29IArP2KdEhOK_0TDkdwJlpcZwDMwc"
+auth_users = "5954632422"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "173"
+osowner_users = "5954632422"
 owner_users = [int(num) for num in osowner_users.split(",")]
