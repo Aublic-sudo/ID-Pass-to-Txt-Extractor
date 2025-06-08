cbe1d4275627fcccc2039f1eaccc65f9dad91cf3
Aublic-sudo <rstock937@gmail.com>
2025-06-08 22:38:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
-bot_token = os.getenv("BOT_TOKEN")
+bot_token = os.getenv("7091128043:AAF6_dq4UK7yoBOTKSWrAG05OBdO7Syt4j0")
 
-auth_users = os.getenv("AUTH_USERS", "")  # comma-separated string
+auth_users = os.getenv("AUTH_USERS", "7360968885")  # comma-separated string
 sudo_users = [int(num) for num in auth_users.split(",") if num.strip().isdigit()]
 
-osowner_users = os.getenv("OSOWNER_USERS", "")
+osowner_users = os.getenv("OSOWNER_USERS", "7360968885")
 owner_users = [int(num) for num in osowner_users.split(",") if num.strip().isdigit()]
