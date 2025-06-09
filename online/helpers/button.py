48450d6927e1f1987c6a788d725344599f9a9474
Aublic-sudo <rstock937@gmail.com>
2025-06-09 11:46:08 +0530

    Update button.py

--- online/helpers/button.py
+++ online/helpers/button.py
@@ -3,40 +3,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 keyboard = InlineKeyboardMarkup([
     [
         InlineKeyboardButton("👑 OWNER", url="https://t.me/Aublic"),
-    ],
-    [
-        InlineKeyboardButton("PW", callback_data="PW"),
-        InlineKeyboardButton("Khazana", callback_data="Khazana"),
-        InlineKeyboardButton("Apni", callback_data="Apni")
-    ],
-   
-    [
-        InlineKeyboardButton("patna", callback_data="patna"),
-        InlineKeyboardButton("infopw", callback_data="infopw"),
-        InlineKeyboardButton("khan", callback_data="khan")
-    ],
-    [
-        InlineKeyboardButton("adownload", callback_data="adownload"),
-        InlineKeyboardButton("pro_vision", callback_data="pro_vision"),
-        InlineKeyboardButton("adda_pdf", callback_data="adda_pdf")
-    ],
-    [
-        InlineKeyboardButton("pro_olive", callback_data="pro_olive"),
-        InlineKeyboardButton("pro_jw", callback_data="pro_jw"),
-        InlineKeyboardButton("top", callback_data="top")
-    ],
-    [
-        InlineKeyboardButton("rozgar", callback_data="rozgar"),
-        InlineKeyboardButton("Pyro", callback_data="Pyro"),
-        InlineKeyboardButton("Cancel", callback_data="Cancel")
-    ],
-    [
-        InlineKeyboardButton("Restart", callback_data="Restart"),
-        InlineKeyboardButton("taiyaric", callback_data="taiyaric")
-    ],
-    [
-      
-        InlineKeyboardButton("cp", callback_data="cp"),
-        InlineKeyboardButton("shell", callback_data="shell"),
-    ],
+    ]
+
 ])
