faa76b6eab8814c104416a1183dcd2acef9f46d6
Aublic-sudo <rstock937@gmail.com>
2025-06-09 09:36:48 +0530

    Update button.py

--- online/helpers/button.py
+++ online/helpers/button.py
@@ -1,48 +1,45 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 keyboard = InlineKeyboardMarkup([
-     [
-            InlineKeyboardButton(
-                text="👑OWNER",
-                url="https://t.me/Aublic",
-            ),
+    [
+        InlineKeyboardButton("👑 OWNER", url="https://t.me/Aublic"),
     ],
     [
-        InlineKeyboardButton("/PW", callback_data="PW"),
-        InlineKeyboardButton("/Khazana", callback_data="Khazana"),
-        InlineKeyboardButton("/Apni", callback_data="Apni")
+        InlineKeyboardButton("PW", callback_data="PW"),
+        InlineKeyboardButton("Khazana", callback_data="Khazana"),
+        InlineKeyboardButton("Apni", callback_data="Apni")
     ],
     [
-        InlineKeyboardButton("/khangs", callback_data="khangs"),
-        InlineKeyboardButton("/cp", callback_data="cp"),
-        InlineKeyboardButton("/exampur", callback_data="exampur")
+        InlineKeyboardButton("khangs", callback_data="khangs"),
+        InlineKeyboardButton("cp", callback_data="cp"),
+        InlineKeyboardButton("exampur", callback_data="exampur")
     ],
     [
-        InlineKeyboardButton("/patna", callback_data="patna"),
-        InlineKeyboardButton("/infopw", callback_data="infopw"),
-        InlineKeyboardButton("/khan", callback_data="khan")
+        InlineKeyboardButton("patna", callback_data="patna"),
+        InlineKeyboardButton("infopw", callback_data="infopw"),
+        InlineKeyboardButton("khan", callback_data="khan")
     ],
     [
-        InlineKeyboardButton("/adownload", callback_data="adownload"),
-        InlineKeyboardButton("/pro_vision", callback_data="pro_vision"),
-        InlineKeyboardButton("/adda_pdf", callback_data="adda_pdf")
+        InlineKeyboardButton("adownload", callback_data="adownload"),
+        InlineKeyboardButton("pro_vision", callback_data="pro_vision"),
+        InlineKeyboardButton("adda_pdf", callback_data="adda_pdf")
     ],
     [
-        InlineKeyboardButton("/pro_olive", callback_data="pro_olive"),
-        InlineKeyboardButton("/pro_jw", callback_data="pro_jw"),
-        InlineKeyboardButton("/top", callback_data="top")
+        InlineKeyboardButton("pro_olive", callback_data="pro_olive"),
+        InlineKeyboardButton("pro_jw", callback_data="pro_jw"),
+        InlineKeyboardButton("top", callback_data="top")
     ],
     [
-        InlineKeyboardButton("/rozgar", callback_data="rozgar"),
-        InlineKeyboardButton("/Pyro", callback_data="Pyro"),
-        InlineKeyboardButton("/Cancel", callback_data="Cancel")
+        InlineKeyboardButton("rozgar", callback_data="rozgar"),
+        InlineKeyboardButton("Pyro", callback_data="Pyro"),
+        InlineKeyboardButton("Cancel", callback_data="Cancel")
     ],
     [
-        InlineKeyboardButton("/Restart", callback_data="Restart"),
-        InlineKeyboardButton("/taiyaric", callback_data="taiyaric")
+        InlineKeyboardButton("Restart", callback_data="Restart"),
+        InlineKeyboardButton("taiyaric", callback_data="taiyaric")
     ],
     [
-        InlineKeyboardButton("/shell", callback_data="shell"),
-        InlineKeyboardButton("/eval", callback_data="eval")
+        InlineKeyboardButton("shell", callback_data="shell"),
+        InlineKeyboardButton("eval", callback_data="eval")
     ]
 ])
